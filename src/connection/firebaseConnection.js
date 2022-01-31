72b49865f3463bfcf5f19af2a4094e0102c630d2
Elton Hugo <elton_has@hotmail.com>
2022-01-31 18:42:19 -0300

    remoção variaveis de ambiente

--- src/connection/firebaseConnection.js
+++ src/connection/firebaseConnection.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAmEKdKG8UebthiIu8HTj5kpIPJXyNoLkU',
-  authDomain: 'topicosespeciaislj.firebaseapp.com',
-  projectId: 'topicosespeciaislj',
-  storageBucket: 'topicosespeciaislj.appspot.com',
-  messagingSenderId: '932310873542',
-  appId: '1:932310873542:web:07a078ae8581c0dcb36ec7'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 let firebaseApp;
